89eff4a12323fd35f1087a7cffcad3e542bbcdc6
Scott Corgan <scottcorgan@gmail.com>
2013-10-15 15:22:07 -0700

    Watch file changes from inside the server file

--- bin/superstatic.js
+++ bin/superstatic.js
@@ -1,32 +1,16 @@
 #!/usr/bin/env node
 
 var path = require('path');
-var Superstatic = require('superstatic-server');
-var gaze = require('gaze');
-var colors = require('colors');
 var argv = require('optimist').argv;
+var Superstatic = require('superstatic-server');
 var defaults = require('../lib/defaults');
 var server = require('../lib/server');
 
 // app working directory
+var port = exports.port =  argv.port || argv.p || defaults.PORT;
+var host = exports.host = argv.host || argv.h || defaults.HOST;
 var awd = exports.awd = (argv._[0])
  ? path.resolve(process.cwd(), argv._[0])
  : defaults.DIRECTORY;
  
-var port = exports.port =  argv.port || argv.p || defaults.PORT;
-var host = exports.host = argv.host || argv.h || defaults.HOST;
-var staticServer = server.createInstance(awd, host, port);
-
-gaze(path.resolve(awd, '**'), function (err, watcher) {
-  var self = this;
-  
-  server.start(staticServer, function () {
-    self.on('all', function (event, filePath) {
-      console.log('\n\n');
-      console.log(event.green + ': ' + filePath);
-      
-      server.restart(staticServer);
-    });
-  });
-  
-});
+server.watch(server.createInstance(awd, host, port), path.resolve(awd, '**'));
