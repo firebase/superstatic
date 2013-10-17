7801b5d295aa65ebe6f9c129b7b58d10d66b39ec
Scott Corgan <scottcorgan@gmail.com>
2013-10-17 12:37:14 -0700

    Nodemon test commit

--- bin/superstatic.js
+++ bin/superstatic.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 
+var spawn = require('child_process').spawn;
 var path = require('path');
 var argv = require('optimist').argv;
 var defaults = require('../lib/defaults');
-var cli = require('../lib/cli');
+var startCli = require('../lib/cli');
 
 // app working directory
 var port = exports.port =  argv.port || argv.p || defaults.PORT;
@@ -11,5 +12,32 @@ var host = exports.host = argv.host || argv.h || defaults.HOST;
 var awd = exports.awd = (argv._[0])
  ? path.resolve(process.cwd(), argv._[0])
  : defaults.DIRECTORY;
- 
- cli.watch(cli.createInstance(awd, host, port), path.resolve(awd, '**'));
+
+
+var nodemonCmd = path.resolve(__dirname, '../node_modules/.bin/nodemon');
+var appDir = path.resolve(__dirname, '../lib/cli.js');
+var watcher = spawn(nodemonCmd, [appDir, awd, '--watch', awd, '-e', 'json']);
+
+watcher.stderr.on('data', function (data) {
+  console.log('stderr: ' + data);
+});
+
+watcher.stdout.on('data', function (data) {
+  process.stdout.write(data.toString());
+});
+
+watcher.on('error', function () {
+  console.log('ERRORRRRR:', arguments);
+});
+
+watcher.on('close', function () {
+  console.log('CLOSED');
+});
+
+
+
+
+startCli(awd, host, port);
+
+// cli.watch(cli.createInstance(awd, host, port), path.resolve(awd, '**'));
+
