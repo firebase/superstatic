5ec02f2a717a1f643e8f9fbfefc8a1f152f5a340
Scott Corgan <scottcorgan@gmail.com>
2013-10-24 15:16:47 -0700

    Local file store test coverage. Ref #29

---
+++ test/server/store/local.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var expect = require('expect.js');
+var Local = require('../../../lib/server/store/local');
+
+describe.only('File store - local', function() {
+  beforeEach(function () {
+    this.local = new Local({});
+    this.filePath = path.resolve(__dirname, '../../fixtures/sample_app/index.html');
+  });
+  
+  it('sets the cwd by default', function () {
+    expect(this.local.cwd).to.be(process.cwd());
+  });
+  
+  it('sets the mime type of a file', function () {
+    expect(this.local.get(this.filePath).type).to.be('text/html');
+  });
+  
+  it('streams the file contents from the given path', function () {
+    expect(this.local.get(this.filePath).pipe).to.not.be(undefined);
+  });
+})
