10467cc227e1b9a9ef8e4c13a4afd1f72d6ea10e
Scott Corgan <scottcorgan@gmail.com>
2013-10-24 14:30:01 -0700

    settings - local file test coverage. Ref #29

---
+++ test/server/config/file.js
@@ -0,0 +1,41 @@
+var path = require('path');
+var expect = require('expect.js');
+var sinon = require('sinon');
+var File = require('../../../lib/server/config/file');
+var fileOptions = {
+  // file: 'superstatic.json',
+  cwd: path.resolve(__dirname, '../../fixtures/sample_app')
+};
+
+describe.only('File - local settings', function() {
+  beforeEach(function () {
+    this.file = new File(fileOptions);
+  });
+  
+  it('uses "superstatic.json" as the default config file', function () {
+    expect(this.file.file).to.be('superstatic.json');
+  });
+  
+  it('loads the config file on instantiation', function () {
+    expect(this.file.configuration).to.have.keys(['name', 'root', 'files']);
+  });
+  
+  it('loads the configuration file', function () {
+    var configFile = this.file.loadConfigurationFile();
+    expect(configFile).to.have.keys(['name', 'root', 'clean_urls']);
+  });
+  
+  it('loads the file list', function () {
+    var config = this.file.loadConfigurationFile();
+    var fileList = this.file.loadFileList(config);
+    expect(fileList).to.be.an('array');
+    expect(fileList).to.contain('/index.html');
+  });
+  
+  it('loads a server config object', function (done) {
+    this.file.load(null, function (err, config) {
+      expect(config).to.have.keys(['root', 'cwd', 'config']);
+      done();
+    });
+  });
+})
