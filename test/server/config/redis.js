0f3bb67d07f7fc8497380af7db1da9a1ba0eba8f
Scott Corgan <scottcorgan@gmail.com>
2013-12-05 14:15:32 -0800

    All tests passing again.
    Still need to add more tests, but a thorough “integration” test passes
    for the new Release cache config

--- test/server/config/redis.js
+++ test/server/config/redis.js
@@ -1,6 +1,5 @@
 var expect = require('expect.js');
 var redis = require('redis-mock');
-// var redis = require('redis');
 var Mocksy = require('mocksy');
 var mocksy = new Mocksy({port:8765});
 var ConfigRedis = require('../../../lib/server/config/redis');
@@ -8,39 +7,24 @@ var redisOptions = {
   client: redis.createClient()
 };
 
-
-
-
-
-// process.env.API_HOST = 'http://localhost:8765';
-// process.env.API_HOST = 'https://api.divshot.com';
-// var config = new ConfigRedis(redisOptions);
-
-// config.load('scottcorgan.divshot.io', function (err, release) {
-  // console.log('done', release);
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-describe.only('Redis - remote settings', function() {
+describe('Redis - remote settings', function() {
   beforeEach(function (done) {
     var self = this;
     
     process.env.API_HOST = 'http://localhost:8765';
     this.redis = new ConfigRedis(redisOptions);
     
-    this.redis.client.set('app.hostname.com', '1234', function () {
-      self.redis.client.set('1234', JSON.stringify({}), function () {
-        mocksy.start(done);
+    this.redis.client.set('host:app.hostname.com:build', 'builds:1234', function () {
+      self.redis.client.set('builds:1234', JSON.stringify({
+        id: '1234',
+        config: {},
+        files: ['/index.html']
+      }), function () {
+        self.redis.client.set('host:app.hostname.com:env', 'apps:5678:production', function () {
+          self.redis.client.set('apps:5678:production', JSON.stringify({}), function () {
+            mocksy.start(done);
+          });
+        });
       });
     });
   });
@@ -49,82 +33,36 @@ describe.only('Redis - remote settings', function() {
     mocksy.stop(done);
   });
   
-//   // it('gets the raw build from the api for a hostname', function (done) {
-//   //   this.redis.getRawBuild('app.hostname.com', function (err, response) {
-//   //     expect(response.method).to.be('GET');
-//   //     expect(response.url).to.be('/builds/lookup?host=app.hostname.com');
-//   //     done();
-//   //   });
-//   // });
-  
-//   // it('gets a build id by hostname', function (done) {
-//   //   this.redis.getBuildId('app.hostname.com', function (err, buildId) {
-//   //     expect(buildId).to.eql('1234');
-//   //     done();
-//   //   });
-//   // });
+  // TODO: need to test the key factory methods (_buildCacheKey(), etc.)
+  // and need to test various other "whitebox pieces"
   
   it('loads the build object from the cache', function (done) {
     var self = this;
-    this.redis.client.set('1234', JSON.stringify({ id: '1234', config: {} }), function () {
-      self.redis.load('app.hostname.com', function (err, build) {
-        expect(build).to.have.keys(['id', 'root', 'host', 'buildId', 'cwd', 'routes', 'max_age']);
-        expect(self.redis.build).to.eql(build);
-        done();
-      });
+    
+    this.redis.load('app.hostname.com', function (err, release) {
+      expect(release).to.have.keys([
+        'id',
+        'root',
+        'host',
+        'buildId',
+        'cwd',
+        'routes',
+        'max_age',
+        'config',
+        'files'
+      ]);
+      
+      done();
     });
   });
   
-//   // it('determines of a given file path is a file', function (done) {
-//   //   var self = this;
-//   //   this.redis.client.set('1234', JSON.stringify({ id: '1234', config: {}, files: ['/index.html'] }), function () {
-//   //     self.redis.load('app.hostname.com', function (err, build) {
-//   //       expect(self.redis.isFile('/index.html')).to.be(true);
-//   //       expect(self.redis.isFile('/dir')).to.be(false);
-//   //       expect(self.redis.isFile('/file.html')).to.be(false);
-//   //       done();
-//   //     });
-//   //   });
-//   // });
-  
-//   // describe('constructing a build object', function() {
-//   //   beforeEach(function (done) {
-//   //     var self = this;
-      
-//   //     this.redis.getRawBuild = function (hostname, callback) {
-//   //       callback(null, {
-//   //         id: '1234',
-//   //         config: {}
-//   //       });
-//   //     };
-      
-//   //     this.redis.constructBuildObject('app.hostname.com', function (err, buildId) {
-//   //       self.buildId = buildId;
-//   //       done();
-//   //     });
-//   //   });
-    
-//   //   it('returns the build id', function () {
-//   //     expect(this.buildId).to.be('1234');
-//   //   });
-    
-//   //   it('sets the key hostname with a value of buildId in the cache', function (done) {
-//   //     this.redis.client.get('app.hostname.com', function (err, buildId) {
-//   //       expect(buildId).to.eql('1234');
-//   //       done();
-//   //     });
-//   //   });
-    
-//   //   it('set the key buildId with the value of build oject in the cache', function (done) {
-//   //     this.redis.client.get('1234', function (err, build) {
-//   //       expect(JSON.parse(build)).to.eql({
-//   //         id: '1234',
-//   //         config: {
-//   //           root: './'
-//   //         }
-//   //       });
-//   //       done();
-//   //     });
-//   //   });
-//   // });
+  it('determines of a given file path is a file', function (done) {
+    var self = this;
+    this.redis.load('app.hostname.com', function (err, build) {
+      expect(self.redis.isFile('/index.html')).to.be(true);
+      expect(self.redis.isFile('/dir')).to.be(false);
+      expect(self.redis.isFile('/file.html')).to.be(false);
+      done();
+    });
+  });
 });
