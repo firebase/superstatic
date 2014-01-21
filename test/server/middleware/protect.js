b589532ac35d42768eb7bd6ce2eb6c2cfb0ada7c
Scott Corgan <scott@divshot.com>
2014-01-21 12:20:09 -0800

    Better test coverage for protect middleware

--- test/server/middleware/protect.js
+++ test/server/middleware/protect.js
@@ -1,63 +1,72 @@
-var setup = require('./_setup');
-var expect = require('expect.js');
-var sinon = require('sinon');
+var connect = require('connect');
+var request = require('supertest');
 var protect = require('../../../lib/server/middleware/protect');
+var defaultSettings = require('../../../lib/server/settings/default');
 
 describe('protect middleware', function() {
+  var app;
+  var settings;
+  
   beforeEach(function () {
-    this.protect = protect();
-    setup.configure(this);
+    app = connect();
+    settings = defaultSettings.create();
+    settings.build = { env: { config: { auth: 'username:password' } } };
+  });
+  
+  it('skips middleware if environment is not protected', function (done) {
+    settings.build = { env: { config: { auth: undefined } } };
+    app.use(protect(settings));
     
-    this.req.settings.build = {
-      env: {
-        config: {
-          auth: 'username:password'
-        }
-      }      
-    };
+    request(app)
+      .get('/')
+      .expect(404)
+      .end(done);
   });
   
-  it('parses the basic auth header', function () {
-    this.req.headers.authorization = 'Basic ' + new Buffer('username:password').toString('base64');
-    expect(protect.auth(this.req, this.res, this.next)).to.equal('username:password');
+  it('skips middleware if there is no build for this environment', function (done) {
+    settings.build = undefined;
+    app.use(protect(settings));
+    
+    request(app)
+      .get('/')
+      .expect(404)
+      .end(done);
   });
   
-  it('ignores auth headers if non exist', function () {
-    this.req.headers.authorization = undefined;
-    expect(protect.auth(this.req)).to.equal(undefined);
+  it('requires http basic auth when environment is protected', function (done) {
+    app.use(protect(settings));
+    
+    request(app)
+      .get('/')
+      .expect(401)
+      .expect('WWW-Authenticate', 'Basic realm="Secure Area"')
+      .end(done);
   });
   
-  it('skips middleware if environment is not protected', function () {
-    this.req.settings.build = { env: { config: { auth: undefined } } };
-    this.protect(this.req, this.res, this.next);
-    expect(this.next.called).to.be(true);
+  it('authorizes request if basic auth credentials match enviroment credentials', function (done) {
+    app.use(function (req, res, next) {
+      req.headers.authorization = 'Basic ' + new Buffer('username:password').toString('base64');
+      next();
+    });
+    app.use(protect(settings));
+    
+    request(app)
+      .get('/')
+      .expect(404)
+      .end(done);
   });
   
-  it('skips middleware if there is no build for this environment', function () {
-    this.req.settings.build = undefined;
-    this.protect(this.req, this.res, this.next);
-    expect(this.next.called).to.be(true);
-  });
-  
-  it('requires http basic auth when environment is protected', function () {
-    this.protect(this.req, this.res, this.next);
-    expect(this.res.statusCode).to.equal(401);
-    expect(this.res.setHeader.calledWith('WWW-Authenticate', 'Basic realm="Secure Area"')).to.equal(true);
-    expect(this.res.end.called).to.equal(true);
-  });
-  
-  it('authorizes request if basic auth credentials match enviroment credentials', function () {
-    this.req.headers.authorization = 'Basic ' + new Buffer('username:password').toString('base64');
-    this.protect(this.req, this.res, this.next);
-    expect(protect.auth(this.req)).to.equal('username:password');
-    expect(this.next.called).to.equal(true);
-  });
-  
-  it('requires authentication if auth is provided invalid credentials', function () {
-    this.req.headers.authorization = 'Basic ' + new Buffer('username:notpassword').toString('base64');
-    this.protect(this.req, this.res, this.next);
-    expect(this.res.statusCode).to.equal(401);
-    expect(this.res.setHeader.calledWith('WWW-Authenticate', 'Basic realm="Secure Area"')).to.equal(true);
-    expect(this.res.end.called).to.equal(true);
+  it('requires authentication if auth is provided invalid credentials', function (done) {
+    app.use(function (req, res, next) {
+      req.headers.authorization = 'Basic ' + new Buffer('username:wrongpassword').toString('base64');
+      next();
+    });
+    app.use(protect(settings));
+    
+    request(app)
+      .get('/')
+      .expect(401)
+      .expect('WWW-Authenticate', 'Basic realm="Secure Area"')
+      .end(done);
   });
 });
