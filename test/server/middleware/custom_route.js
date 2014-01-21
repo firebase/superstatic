5884d8c6ba481306ffea619a304cf0831207945c
Scott Corgan <scott@divshot.com>
2014-01-21 11:29:09 -0800

    More test coverage for custom route middleware

--- test/server/middleware/custom_route.js
+++ test/server/middleware/custom_route.js
@@ -1,75 +1,118 @@
 var path = require('path');
+var connect = require('connect');
+var request = require('supertest');
 var expect = require('expect.js');
 var setup = require('./_setup');
 var customRoute = require('../../../lib/server/middleware/custom_route');
+var defaultFileStore = require('../../../lib/server/store/default');
+var defaultSettings = require('../../../lib/server/settings/default');
+var defaultRoutes = {
+  '/test1': '/index.html',
+  '/test3': '/test/dir'
+};
 
 describe('custom route middleware', function() {
+  var app;
+  var fileStore;
+  var settings;
+  
   beforeEach(function () {
-    this.customRoute = customRoute();
-    setup.configure(this);
+    app = connect();
+    fileStore = defaultFileStore.create();
+    settings = defaultSettings.create();
+    settings.configuration.routes = defaultRoutes
     
-    this.req.config.routes = {
-      'test1': 'superstatic.html',
-      'test2': 'superstatic.html',
-      'test3': 'test/dir'
-    };
+    app.use(function (req, res, next) {
+      res.send = function (pathname) {
+        res.writeHead(200);
+        res.end(pathname);
+      }
+      req.config = settings.configuration;
+      
+      next();
+    });
   });
   
-  it('serves the mapped route file for a custom route', function () {
-    this.req.ss.pathname = '/test1';
-    this.customRoute(this.req, this.res, this.next);
-    expect(this.res.send.calledWith('/superstatic.html')).to.equal(true);
+  it('serves the mapped route file for a custom route', function (done) {
+    app.use(customRoute(settings, fileStore));
+    
+    request(app)
+      .get('/test1')
+      .expect(200)
+      .expect('/index.html')
+      .end(done);
   });
   
-  it('serves the index file of a directory if mapped route is mapped to a directory', function () {
-    this.req.ss.pathname = '/test3';
-    this.customRoute(this.req, this.res, this.next);
-    expect(this.res.send.calledWith('/test/dir/index.html')).to.equal(true);
+  it('serves the index file of a directory if mapped route is mapped to a directory', function (done) {
+    app.use(customRoute(settings, fileStore));
+    
+    request(app)
+      .get('/test3')
+      .expect(200)
+      .expect('/test/dir/index.html')
+      .end(done);
   });
   
-  it('serves the mapped route file for a custom route with a declared root', function () {
-    var self = this;
-    this.req.ss.pathname = '/test1';
-    this.req.config.root = './public';
-    this.req.rootPathname = function (pathname) {
-      return path.join('/', self.req.config.root, pathname);
+  it('serves the mapped route file for a custom route with a declared root', function (done) {
+    settings.rootPathname = function (pathname) {
+      var root = this.configuration.root || './';
+      return path.join('/', root, pathname);
     };
     
-    this.customRoute(this.req, this.res, this.next);
-    expect(this.res.send.calledWith('/public/superstatic.html')).to.equal(true);
+    app.use(function (req, res, next) {
+      req.config.root = './public';
+      next();
+    });
+    app.use(customRoute(settings, fileStore));
+    
+    request(app)
+      .get('/test1')
+      .expect(200)
+      .expect('/public/index.html')
+      .end(done);
   });
   
-  it('skips the middleware if there is no custom route', function () {
-    this.customRoute(this.req, this.res, this.next);
-    expect(this.next.called).to.equal(true);
-    expect(this.res.send.called).to.equal(false);
+  it('skips the middleware if there is no custom route', function (done) {
+    settings.configuration.routes = {};
+    app.use(customRoute(settings, fileStore));
+    
+    request(app)
+      .get('/no-route')
+      .expect(404)
+      .end(done);
   });
   
-  it('skips the middleware if the custom route is for a file that does not exist', function () {
-    this.req.settings.isFile = function () { return false; };
-    this.customRoute(this.req, this.res, this.next);
-    expect(this.next.called).to.equal(true);
-    expect(this.res.send.called).to.equal(false);
+  it('skips the middleware if the custom route is for a file that does not exist', function (done) {
+    fileStore.exists = function () {return false;}
+    app.use(customRoute(settings, fileStore));
+    
+    request(app)
+      .get('/test1')
+      .expect(404)
+      .end(done);
   });
   
   describe('glob matching', function() {
-    it('maps all paths to the same pathname', function () {
-      this.req.ss.pathname = '/anything/can/work';
-      this.req.config.routes = {
-        '**': 'superstatic.html'
-      };
+    it('maps all paths to the same pathname', function (done) {
+      settings.configuration.routes = {'**': 'index.html'};
+      app.use(customRoute(settings, fileStore));
       
-      this.customRoute(this.req, this.res, this.next);
-      expect(this.res.send.calledWith('/superstatic.html'));
+      request(app)
+        .get('/any-route')
+        .expect(200)
+        .expect('/index.html')
+        .end(done);
     });
     
-    it('maps all requests to files in a given directory to the same pathname', function () {
-      this.req.ss.pathname = '/subdir/anything/here';
-      this.req.config.routes = {
-        'subdir/**': 'superstatic.html'
-      };
-      this.customRoute(this.req, this.res, this.next);
-      expect(this.res.send.calledWith('/superstatic.html')).to.equal(true);
+    it('maps all requests to files in a given directory to the same pathname', function (done) {
+      settings.configuration.routes = {'subdir/**': 'index.html'};
+      app.use(customRoute(settings, fileStore));
+      
+      request(app)
+        .get('/subdir/anything/here')
+        .expect(200)
+        .expect('/index.html')
+        .end(done);
     });
-  })
+  });
 });
