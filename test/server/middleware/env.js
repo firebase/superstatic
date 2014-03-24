bdca203e623d4590c22afc7d670bcbf5f8994388
Scott Corgan <scott@divshot.com>
2014-03-24 09:39:03 -0700

    Add test for javascript response

--- test/server/middleware/env.js
+++ test/server/middleware/env.js
@@ -3,25 +3,19 @@ var request = require('supertest');
 var env = require('../../../lib/server/middleware/env');
 var defaultSettings = require('../../../lib/server/settings/default');
 var util = require('util');
+var clone = require('clone');
 
 describe('env middleware', function() {
   var app;
   var settings;
   
-  context('with local env', function() {
+  describe('with local env', function() {
     beforeEach(function () {
       app = connect();
       settings = defaultSettings.create();
-      settings.build = {
-        env: {
-          config: {
-            auth: 'username:password',
-            env: {
-              key1: 'value1'
-            }
-          }
-        }
-      };
+      settings.build = appBuild({
+        key1: 'value1'
+      });
       app.use(env(settings, {local:'environment'}));
     });
 
@@ -40,51 +34,46 @@ describe('env middleware', function() {
 
   });
 
-  context('with no local env', function() {
+  describe('with no local env', function() {
     beforeEach(function () {
       app = connect();
       settings = defaultSettings.create();
-      settings.build = {
-        env: {
-          config: {
-            auth: 'username:password',
-            env: {
-              key1: 'value1',
-              key2: 'value2'
-            }
-          }
-        }
-      };
+      settings.build = appBuild({
+        key1: 'value1',
+        key2: 'value2'
+      });
       app.use(env(settings));
     });
     
-    it('intercepts the request for the environment javascript file');
-    // Not sure how to test the response body here.
-    // response.body is an Object.
-    // , function (done) {
-    //   request(app)
-    //     .get('/__/env')
-    //     .expect('Content-Type', 'text/javascript')
-    //     .expect(200)
-    //     .expect(function (response) {
-    //       if (response.body != 'this.__env = ' + JSON.stringify(settings.build.env.config.env) + ';') {
-    //         return 'wrong js content: ' + JSON.stringify(response.body);
-    //       }
-    //     })
-    //     .end(done);
-    // });
+    it('intercepts the request for the environment javascript file', function (done) {
+      request(app)
+        .get('/__/env.js')
+        .expect('Content-Type', 'text/javascript')
+        .expect(200)
+        .expect('this.__env = {"key1":"value1","key2":"value2"};')
+        .end(done);
+    });
 
     it('intercepts the request for the environment json file', function (done) {
       request(app)
         .get('/__/env.json')
         .expect('Content-Type', 'application/json')
         .expect(200)
-        .expect(function (response) {
-          if (JSON.stringify(response.body) != JSON.stringify(settings.build.env.config.env)) { 
-            return 'wrong json content: ' + JSON.stringify(response.body);
-          }
+        .expect({
+          key1: 'value1',
+          key2: 'value2'
         })
         .end(done);
     });
   });
 });
+
+function appBuild (env) {
+  return clone({
+    env: {
+      config: {
+        env: clone(env)
+      }
+    }
+  });
+}
