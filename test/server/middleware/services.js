4738bdf04cf1e15b9036ad0408a4a347e2de6c61
Scott Corgan <scott@divshot.com>
2014-04-28 16:13:33 -0700

    Skip middleware tests

--- test/server/middleware/services.js
+++ test/server/middleware/services.js
@@ -10,91 +10,91 @@ describe.only('services middleware', function () {
     app = connect();
   });
   
-  // describe.skip('skipping middleware', function () {
-    
-  //   beforeEach(function () {
-  //     var serviceList = {
-  //       service1: 'test'
-  //     };
-      
-  //     app.use(services(serviceList, '__'));
-  //   });
-    
-  //   it('skips if the route is not a service request', function (done) {
-  //     request(app)
-  //       .get('/')
-  //       .expect(404)
-  //       .end(done);
-  //   });
-    
-  //   it('skips if the service does not exist', function (done) {
-  //     request(app)
-  //       .get('/__/no-service')
-  //       .expect(404)
-  //       .end(done);
-  //   });
-    
-  //   it('skips if the requesting app is not configured to use the middleware', function (done) {
-  //     request(app)
-  //       .get('/__/service1/test')
-  //       .expect(404)
-  //       .end(done);
-  //   });
-    
-  // });
-  
-  describe.skip('running services', function () {
-    var service1Ran = false;
-    var service2Ran = false;
+  describe('skipping middleware', function () {
     
     beforeEach(function () {
-      service1Ran = false;
-      service2Ran = false;
-      
       var serviceList = {
-        service1: function (req, res, next) {
-          service1Ran = true;
-          next();
-        },
-        service2: function (req, res, next) {
-          service2Ran = true;
-          res.writeHead(200);
-          res.write('service2');
-          res.end();
-        }
+        service1: 'test'
       };
       
-      app.use(setConfig);
-      app.use(services(serviceList, '__'));  
+      app.use(services(serviceList, '__'));
     });
     
-    it('runs the service if it is configured in the app config', function (done) {
+    it('skips if the route is not a service request', function (done) {
       request(app)
-        .get('/__/service1/test')
-        .expect(200)
-        .expect('service2')
-        .expect(function () {
-          expect(service1Ran).to.equal(true);
-          expect(service2Ran).to.equal(true);
-        })
+        .get('/')
+        .expect(404)
         .end(done);
     });
     
-    function setConfig (req, res, next) {
-      req.config = {
-        services: {
-          noService: {},
-          service1: {
-            test1: {}
-          },
-          service2: {
-            test2: {}
-          }
-        }
-      };
-      next();
-    }
+    it('skips if the service does not exist', function (done) {
+      request(app)
+        .get('/__/no-service')
+        .expect(404)
+        .end(done);
+    });
+    
+    it('skips if the requesting app is not configured to use the middleware', function (done) {
+      request(app)
+        .get('/__/service1/test')
+        .expect(404)
+        .end(done);
+    });
     
   });
+  
+  // describe('running services', function () {
+  //   var service1Ran = false;
+  //   var service2Ran = false;
+    
+  //   beforeEach(function () {
+  //     service1Ran = false;
+  //     service2Ran = false;
+      
+  //     var serviceList = {
+  //       service1: function (req, res, next) {
+  //         service1Ran = true;
+  //         next();
+  //       },
+  //       service2: function (req, res, next) {
+  //         service2Ran = true;
+  //         res.writeHead(200);
+  //         res.write('service2');
+  //         res.end();
+  //       }
+  //     };
+      
+  //     app.use(setConfig);
+  //     app.use(services(serviceList, '__'));  
+  //   });
+    
+  //   it('runs the service if it is configured in the app config', function (done) {
+  //     request(app)
+  //       .get('/__/service1/test')
+  //       .expect(200)
+  //       .expect('service2')
+  //       .expect(function () {
+  //         expect(service1Ran).to.equal(true);
+  //         expect(service2Ran).to.equal(true);
+  //       })
+  //       .end(done);
+  //   });
+    
+  //   function setConfig (req, res, next) {
+  //     req.config = {
+  //       services: {
+  //         noService: {},
+  //         service1: {
+  //           test1: {}
+  //         },
+  //         service2: {
+  //           test2: {}
+  //         }
+  //       }
+  //     };
+  //     next();
+  //   }
+    
+  // });
   
 });
