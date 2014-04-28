e8a2611f0aeddfbf25b2d75cfd4422d788aae737
Scott Corgan <scott@divshot.com>
2014-04-28 16:31:32 -0700

    Renew test coverage for services
    WIP: Still need to allow service to determine if it should activate

--- test/server/middleware/services.js
+++ test/server/middleware/services.js
@@ -43,58 +43,53 @@ describe.only('services middleware', function () {
     
   });
   
-  // describe('running services', function () {
-  //   var service1Ran = false;
-  //   var service2Ran = false;
+  describe('running services', function () {
+    var service1Ran = false;
+    var service2Ran = false;
     
-  //   beforeEach(function () {
-  //     service1Ran = false;
-  //     service2Ran = false;
+    beforeEach(function () {
+      service1Ran = false;
+      service2Ran = false;
       
-  //     var serviceList = {
-  //       service1: function (req, res, next) {
-  //         service1Ran = true;
-  //         next();
-  //       },
-  //       service2: function (req, res, next) {
-  //         service2Ran = true;
-  //         res.writeHead(200);
-  //         res.write('service2');
-  //         res.end();
-  //       }
-  //     };
+      var serviceList = {
+        service2: function (req, res, next) {
+          service2Ran = true;
+          res.writeHead(200);
+          res.write('service2');
+          res.end();
+        }
+      };
       
-  //     app.use(setConfig);
-  //     app.use(services(serviceList, '__'));  
-  //   });
+      app.use(setConfig);
+      app.use(services(serviceList, '__'));  
+    });
     
-  //   it('runs the service if it is configured in the app config', function (done) {
-  //     request(app)
-  //       .get('/__/service1/test')
-  //       .expect(200)
-  //       .expect('service2')
-  //       .expect(function () {
-  //         expect(service1Ran).to.equal(true);
-  //         expect(service2Ran).to.equal(true);
-  //       })
-  //       .end(done);
-  //   });
+    it('runs the service if it is configured in the app config', function (done) {
+      request(app)
+        .get('/__/service2/test2')
+        .expect(200)
+        .expect('service2')
+        .expect(function () {
+          expect(service2Ran).to.equal(true);
+        })
+        .end(done);
+    });
     
-  //   function setConfig (req, res, next) {
-  //     req.config = {
-  //       services: {
-  //         noService: {},
-  //         service1: {
-  //           test1: {}
-  //         },
-  //         service2: {
-  //           test2: {}
-  //         }
-  //       }
-  //     };
-  //     next();
-  //   }
+    function setConfig (req, res, next) {
+      req.config = {
+        services: {
+          // noService: {},
+          // service1: {
+          //   test1: {}
+          // },
+          service2: {
+            test2: {}
+          }
+        }
+      };
+      next();
+    }
     
-  // });
+  });
   
 });
