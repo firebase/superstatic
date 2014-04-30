28fba010927f66a137792ef3388f60934f4e7a17
Kyle MacDonald <kyle@carrotcreative.com>
2014-04-30 17:56:53 -0400

    update test requires for lib/index

--- test/superstatic.js
+++ test/superstatic.js
@@ -1,5 +1,5 @@
 var expect = require('expect.js');
-var superstatic = require('../lib/superstatic');
+var superstatic = require('../lib');
 var SuperstaticServer = require('../lib/server');
 var ignore = require('../lib/ignore');
 var defaultSettings = require('../lib/server/settings/default');
@@ -8,11 +8,11 @@ describe('Exposing superstatic', function() {
   it('exposes Superstatic server', function () {
     expect(superstatic.Server).to.eql(SuperstaticServer);
   });
-  
+
   it('exposes file globs to ignore', function () {
     expect(superstatic.ignore).to.eql(ignore);
   });
-  
+
   it('exposes the default settings', function () {
     expect(superstatic.defaultSettings).to.eql(defaultSettings);
   });
