5e2818ed90a6dd5fb2550141d7539529d8badc49
Scott Corgan <scottcorgan@gmail.com>
2014-05-06 14:01:06 -0700

    Feature: adds redirect middleware. Ref #50

--- README.md
+++ README.md
@@ -56,6 +56,31 @@ apps. An example:
 }
 ```
 
+**redirects** you can specify to have certain url paths be redirected (specifying a custom http status code, or which defaults to 301) to other url paths by supplying an object to the `redirects` key. Route path matching is similar to using custom routes. For example:
+
+Default 301 redirect
+
+```json
+{
+  "redirects": {
+    "/some/old/path": "/some/new/path"
+  }
+}
+```
+
+Custom http status code
+
+```json
+{
+  "redirects": {
+    "/some/old/path": {
+      "statusCode": 302,
+      "url": "/some/new/path"
+    }
+  }
+}
+```
+
 **error_page:** the path to the page that you want to render 404 errors if an unrecognized
 URL is supplied. For example, `error.html`.
 
---
+++ test/server/middleware/redirect.js
@@ -0,0 +1,100 @@
+var expect = require('expect.js');
+var connect = require('connect');
+var request = require('supertest');
+var redirect = require('../../../lib/server/middleware/redirect');
+
+describe('redirect middleware', function () {
+  
+  it('skips the middleware if there are no redirects configured', function (done) {
+    var app = connect()
+      .use(function (req, res, next) {
+        req.config = {};
+        next();
+      })
+      .use(redirect());
+    
+    request(app)
+      .get('/')
+      .expect(404)
+      .end(done);
+  });
+  
+  it('skips middleware when there are no matching redirects', function (done) {
+    var app = connect()
+      .use(function (req, res, next) {
+        req.config = {
+          redirects: {
+            '/source': '/redirect'
+          }
+        };
+        next();
+      })
+      .use(redirect());
+    
+    request(app)
+      .get('/none')
+      .expect(404)
+      .end(done);
+  });
+  
+  it('redirects to a configured redirects object with a default status code of 301', function (done) {
+    var app = connect()
+      .use(function (req, res, next) {
+        req.config = {
+          redirects: {
+            '/source': '/redirect'
+          }
+        };
+        next();
+      })
+      .use(redirect());
+    
+    request(app)
+      .get('/source')
+      .expect(301)
+      .expect('location', '/redirect')
+      .end(done);
+  });
+  
+  it('redirects to a configured path with a custom status code', function (done) {
+    var app = connect()
+      .use(function (req, res, next) {
+        req.config = {
+          redirects: {
+            '/source': {
+              statusCode: 302,
+              url: '/redirect'
+            }
+          }
+        };
+        next();
+      })
+      .use(redirect());
+    
+    request(app)
+      .get('/source')
+      .expect(302)
+      .expect('location', '/redirect')
+      .end(done);
+  });
+  
+  it('adds leading slash to all redirect paths', function (done) {
+    var app = connect()
+      .use(function (req, res, next) {
+        req.config = {
+          redirects: {
+            'source': '/redirect' // No slash
+          }
+        };
+        next();
+      })
+      .use(redirect());
+    
+    request(app)
+      .get('/source')
+      .expect(301)
+      .expect('location', '/redirect')
+      .end(done);
+  });
+  
+});
