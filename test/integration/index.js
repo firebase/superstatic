c20f4ecd78addc1a67a4cafefb5af8909a138801
Scott Corgan <scottcorgan@gmail.com>
2014-08-07 11:07:40 -0700

    tests: integration for most middleware

--- test/integration/index.js
+++ test/integration/index.js
@@ -58,19 +58,14 @@ describe('serving requests', function () {
         .expect('Content-Type', 'text/css; charset=UTF-8')
         .end(done);
     });
-    
-    it('serves an error page', function (done) {
-      request(app)
-        .get('/asdfasdf')
-        .expect('error.html')
-        .expect(404)
-        .end(done);
-    });
   });
 
   describe('with settings', function () {
-    describe('redirects', function () {
-      it('to url', function (done) {
+    describe('services', function () {
+      //
+    });
+    
+    it('redirects with 301', function (done) {
         serverWithConfig({
           redirects: {
             '/from': '/to'
@@ -82,6 +77,140 @@ describe('serving requests', function () {
             .expect('Location', '/to')
             .end(endApp(app, done));
         });
+    });
+    
+    describe('trailing slash', function () {
+      it('removes the trailing slash', function (done) {
+        serverWithConfig({}, function (err, app) {
+          request(app)
+            .get('/slash/')
+            .expect(301)
+            .expect('Location', '/slash')
+            .end(endApp(app, done));
+        });
+      });
+      
+      it('keeps the trailing slash with a directory index file', function (done) {
+        serverWithConfig({}, function (err, app) {
+          request(app)
+            .get('/sub/')
+            .expect(200)
+            .expect('sub/index.html')
+            .end(endApp(app, done));
+        });
+      });
+    });
+    
+    describe('basic auth', function () {
+      // TODO: refactor this, then write the tests
+    });
+    
+    it('sets the headers', function (done) {
+      serverWithConfig({
+        headers: {
+          '/test': {
+            'anything': 'here'
+          }
+        }
+      }, function (err, app) {
+        request(app)
+          .get('/test')
+          .expect('anything', 'here')
+          .end(endApp(app, done));
+      });
+    });
+    
+    it('sets the response cache', function (done) {
+      serverWithConfig({
+        cache_control: {
+          '/test': 1234
+        }
+      }, function (err, app) {
+        request(app)
+          .get('/test')
+          .expect('Cache-Control', 'public, max-age=1234')
+          .end(endApp(app, done));
+      });
+    });
+    
+    describe('environment variables', function () {
+      // TODO: test this
+    });
+    
+    describe('clean urls', function () {
+      it('redirects to a clean url when it is an html file', function (done) {
+        serverWithConfig({
+          clean_urls: true
+        }, function (err, app) {
+          request(app)
+            .get('/about.html')
+            .expect(301)
+            .expect('Location', '/about')
+            .end(endApp(app, done));
+        });
+      });
+      
+      it('serves the html file', function (done) {
+        serverWithConfig({
+          clean_urls: true
+        }, function (err, app) {
+          request(app)
+            .get('/about')
+            .expect(200)
+            .expect('about.html')
+            .end(endApp(app, done));
+        });
+      });
+    });
+    
+    describe('custom routes', function () {
+      it('serves custom route', function (done) {
+        serverWithConfig({
+          routes: {
+            '/route': 'index.html'
+          }
+        }, function (err, app) {
+          request(app)
+            .get('/route')
+            .expect(200)
+            .expect('index.html')
+            .end(endApp(app, done));
+        });
+      });
+      
+      it('services static file if no route', function (done) {
+        serverWithConfig({
+          routes: {
+            '/route': 'index.html'
+          }
+        }, function (err, app) {
+          request(app)
+            .get('/about.html')
+            .expect(200)
+            .expect('about.html')
+            .end(endApp(app, done));
+        });
+      });
+    });
+    
+    it('serves default favicon', function (done) {
+      serverWithConfig({}, function (err, app) {
+        request(app)
+          .get('/favicon.ico')
+          .expect(200)
+          .end(endApp(app, done));
+      });
+    });
+    
+    it('serves an error page', function (done) {
+      serverWithConfig({
+        error_page: 'error.html'
+      }, function (err, app) {
+        request(app)
+          .get('/asdfasdf')
+          .expect('error.html')
+          .expect(404)
+          .end(endApp(app, done));
       });
     });
     
@@ -99,7 +228,14 @@ describe('serving requests', function () {
     
     function endApp (app, done) {
       return function (err) {
-        (err) ? done(err) : app.close(done);
+        if (err) {
+          app.close(function () {
+            done(err);
+          });
+        }
+        else {
+          app.close(done);
+        }
       };
     }
   });
