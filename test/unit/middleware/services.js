b773deac9e6b1f153e55ac2c9c1a7e7442f5d464
Scott Corgan <scott@divshot.com>
2014-12-31 12:01:31 -0800

    pass service config into service

--- test/unit/middleware/services.js
+++ test/unit/middleware/services.js
@@ -4,32 +4,14 @@ var connect = require('connect');
 var expect = require('chai').expect;
 
 var services = require('../../../lib/middleware/services');
-var dfs = require('../../../lib/dfs');
-var responder = require('../../../lib/responder');
 
 describe('services', function () {
   
-  var provider = dfs({
-    root: '.tmp'
-  });
   var app;
   
   beforeEach(function () {
     
-    app = connect()
-      .use(function (req, res, next) {
-        
-        responder({
-          res: res,
-          provider: provider
-        });
-        next();
-      });
-  });
-  
-  afterEach(function () {
-    
-    fs.removeSync('.tmp');
+    app = connect();
   });
   
   it('skips with no services given', function (done) {
@@ -42,11 +24,35 @@ describe('services', function () {
       .end(done);
   });
   
+  it('skips if no service in config', function (done) {
+    
+    app.use(services({
+      services: {
+        forms: formsService()
+      },
+      config: {}
+    }));
+    
+    function formsService (spec) {
+      
+      return function (req, res, next) {
+        
+        res.end('forms');
+        next();
+      };
+    }
+    
+    request(app)
+      .get('/__/forms/test')
+      .expect(404)
+      .end(done);
+  });
+  
   it('runs based on url', function (done) {
     
     app.use(services({
       services: {
-        forms: formsService({})
+        forms: formsService()
       },
       config: {
         forms: true
@@ -72,7 +78,7 @@ describe('services', function () {
     
     app.use(services({
       services: {
-        forms: formsService({})
+        forms: formsService()
       },
       config: {
         Forms: true
@@ -94,11 +100,44 @@ describe('services', function () {
       .end(done);
   });
   
-  it.skip('runs based on matches()', function (done) {
+  it('runs based on matches()', function (done) {
     
     app.use(services({
       services: {
-        forms: formsService({})
+        forms: formsService()
+      },
+      config: {
+        forms: true
+      }
+    }));
+    
+    function formsService (spec) {
+      
+      var fn = function (req, res, next) {
+        
+        res.end('forms');
+        next();
+      };
+      
+      fn.matches = function (req) {
+        
+        return true;
+      };
+      
+      return fn;
+    }
+    
+    request(app)
+      .get('/__/forms-matches')
+      .expect('forms')
+      .end(done);
+  });
+  
+  it('gives service config to service', function (done) {
+    
+    app.use(services({
+      services: {
+        forms: formsService()
       },
       config: {
         forms: true
@@ -109,19 +148,49 @@ describe('services', function () {
       
       return function (req, res, next) {
         
-        res.end('forms');
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(req.service));
+      };
+    }
+    
+    request(app)
+      .get('/__/forms/api/testing')
+      .expect({
+        name: 'forms',
+        config: true,
+        path: '/forms/api/testing'
+      })
+      .end(done);
+  });
+  
+  it('removes service config foot print', function (done) {
+    
+    app.use(services({
+      services: {
+        forms: formsService()
+      },
+      config: {
+        forms: true
+      }
+    }));
+    
+    app.use(function (req, res, next) {
+      
+      expect(req.service).to.equal(undefined);
+      next();
+    });
+    
+    function formsService (spec) {
+      
+      return function (req, res, next) {
+        
         next();
       };
     }
     
-    formsService.matches = function (req) {
-      
-      return false;
-    };
-    
     request(app)
-      .get('/__/forms-matches')
-      .expect('forms')
+      .get('/__/forms')
+      .expect(404)
       .end(done);
   });
 });
