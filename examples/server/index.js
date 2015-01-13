5c5ade5d43c17c2bbf5c9ee93eb14db53b06848f
Scott Corgan <scott@divshot.com>
2015-01-13 10:45:29 -0800

    example: add examples

---
+++ examples/server/index.js
@@ -0,0 +1,16 @@
+var superstatic = require('../../lib/server');
+
+var spec = {
+  port: 3474,
+  config: {
+    root: './app'
+  },
+  cwd: __dirname,
+  errorPage: __dirname + '/error.html'
+};
+
+var app = superstatic(spec);
+var server = app.listen(function (err) {
+  
+  console.log('Superstatic now serving on port 3474 ...');
+});
