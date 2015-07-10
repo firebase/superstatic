ce95f8f07ea84169d74337261f53134f4c6ca957
Michael Bleigh <mbleigh@mbleigh.com>
2015-07-09 18:01:25 -0700

    Fix tests.

--- test/unit/middleware/static-router.js
+++ test/unit/middleware/static-router.js
@@ -8,19 +8,19 @@ var dfs = require('../../../lib/dfs');
 var responder = require('../../../lib/responder');
 
 describe('static router', function () {
-  
+
   var provider = dfs({
     root: '.tmp'
   });
   var app;
-  
+
   beforeEach(function () {
-    
+
     fs.outputFileSync('.tmp/index.html', 'index', 'utf8');
-    
+
     app = connect()
       .use(function (req, res, next) {
-        
+
         responder({
           req: req,
           res: res,
@@ -29,20 +29,20 @@ describe('static router', function () {
         next();
       })
   });
-  
+
   afterEach(function () {
-    
+
     fs.removeSync('.tmp');
   });
-  
+
   it('serves a route', function (done) {
-    
+
     app.use(staticRouter({
-      routes: {
+      rewrites: {
         '/my-route': '/index.html'
       }
     }));
-    
+
     request(app)
       .get('/my-route')
       .expect(200)
@@ -50,15 +50,15 @@ describe('static router', function () {
       .expect('content-type', 'text/html; charset=utf-8')
       .end(done);
   });
-  
+
   it('serves a route with a glob', function (done) {
-    
+
     app.use(staticRouter({
-      routes: {
+      rewrites: {
         '**': '/index.html'
       }
     }));
-    
+
     request(app)
       .get('/my-route')
       .expect(200)
@@ -66,15 +66,15 @@ describe('static router', function () {
       .expect('content-type', 'text/html; charset=utf-8')
       .end(done);
   });
-  
+
   it('serves a negated route', function (done) {
-    
+
     app.use(staticRouter({
-      routes: {
+      rewrites: {
         '!/no': '/index.html'
       }
     }));
-    
+
     request(app)
       .get('/my-route')
       .expect(200)
@@ -82,15 +82,15 @@ describe('static router', function () {
       .expect('content-type', 'text/html; charset=utf-8')
       .end(done);
   });
-  
+
   it('skips if no match is found', function (done) {
-    
+
     app.use(staticRouter({
-      routes: {
+      rewrites: {
         '/skip': '/index.html'
       }
     }));
-    
+
     request(app)
       .get('/hi')
       .expect(404)
@@ -98,27 +98,27 @@ describe('static router', function () {
   });
 
   it('ensures matching file extension', function (done) {
-    
+
     app.use(staticRouter({
-      routes: {
+      rewrites: {
         '**': "/index.html"
       }
     }));
-    
+
     request(app)
       .get('/index.js')
       .expect(404)
       .end(done);
   });
-  
+
   describe('uses first match', function () {
-    
+
     beforeEach(function () {
-      
+
       fs.outputFileSync('.tmp/admin/index.html', 'admin index', 'utf8');
-      
+
       app.use(staticRouter({
-        routes: [
+        rewrites: [
           {
             "/admin/**": "/admin/index.html",
             "/something/**": "/something/indexf.html"
@@ -127,27 +127,27 @@ describe('static router', function () {
         ]
       }));
     });
-    
+
     it('first route with 1 depth route', function (done) {
-      
+
       request(app)
         .get('/admin/anything')
         .expect(200)
         .expect('admin index')
         .end(done);
     });
-    
+
     it('first route with 2 depth route', function (done) {
-      
+
       request(app)
         .get('/admin/anything/else')
         .expect(200)
         .expect('admin index')
         .end(done);
     });
-    
+
     it('second route', function (done) {
-      
+
       request(app)
         .get('/anything')
         .expect(200)
