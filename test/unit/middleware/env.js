c69f9973c0f2ed46c40b3b6dc9fca85c0e2cb722
Michael Bleigh <mbleigh@mbleigh.com>
2015-07-17 12:12:48 -0700

    All tests passing with async config loader.

--- test/unit/middleware/env.js
+++ test/unit/middleware/env.js
@@ -7,16 +7,14 @@ var env = require('../../../lib/middleware/env');
 var responder = require('../../../lib/responder');
 
 describe('env', function () {
-  
+
   var app;
   var router;
-  
+
   beforeEach(function () {
-    
-    router = Router();
     app = connect()
       .use(function (req, res, next) {
-        
+
         responder({
           req: req,
           res: res,
@@ -25,18 +23,14 @@ describe('env', function () {
         next();
       });
   });
-  
+
   it('serves json', function (done) {
-    
-    env({
+    app.use(env({
       data: {
         key: 'value'
-      },
-      router: router
-    });
-    
-    app.use(router);
-    
+      }
+    }));
+
     request(app)
       .get('/__/env.json')
       .expect(200)
@@ -46,18 +40,14 @@ describe('env', function () {
       .expect('content-type', 'application/json; charset=utf-8')
       .end(done);
   });
-  
+
   it('serves javascript', function (done) {
-    
-    env({
+    app.use(env({
       data: {
         key: 'value'
-      },
-      router: router
-    });
-    
-    app.use(router);
-    
+      }
+    }));
+
     request(app)
       .get('/__/env.js')
       .expect(200)
