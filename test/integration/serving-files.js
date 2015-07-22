1eb4ce0ef0c434a07648a56cfb80ac2e4434753e
Michael Bleigh <mbleigh@mbleigh.com>
2015-07-22 15:56:09 -0700

    Pending-ify some things.

--- test/integration/serving-files.js
+++ test/integration/serving-files.js
@@ -17,28 +17,28 @@ var options = function () {
 };
 
 describe('serves', function () {
-  
+
   beforeEach(function () {
-    
+
     fs.outputFileSync('.tmp/index.html', 'index', 'utf-8');
     fs.outputFileSync('.tmp/test.html', 'test', 'utf-8');
     fs.outputFileSync('.tmp/app.js', 'console.log("js")', 'utf-8');
     fs.outputFileSync('.tmp/dir/index.html', 'dir index', 'utf-8');
     fs.outputFileSync('.tmp/dir/sub.html', 'dir sub', 'utf-8');
   });
-  
+
   afterEach(function () {
-    
+
     fs.removeSync('.tmp');
   });
-  
+
   it('static file', function (done) {
-    
+
     var opts = options();
-    
+
     var app = connect()
       .use(superstatic(opts));
-    
+
     request(app)
       .get('/test.html')
       .expect(200)
@@ -46,14 +46,14 @@ describe('serves', function () {
       .expect('Content-Type', 'text/html; charset=utf-8')
       .end(done);
   });
-  
+
   it('directory index file', function (done) {
-    
+
     var opts = options();
-    
+
     var app = connect()
       .use(superstatic(opts));
-    
+
     request(app)
       .get('/dir/')
       .expect(200)
@@ -61,29 +61,29 @@ describe('serves', function () {
       .expect('Content-Type', 'text/html; charset=utf-8')
       .end(done);
   });
-  
+
   it('missing directory index', function (done) {
-    
+
     var opts = options();
-    
+
     opts.config.root = './';
-    
+
     var app = connect()
       .use(superstatic(opts));
-    
+
     request(app)
       .get('/')
       .expect(404)
       .end(done);
   });
-  
+
   it('javascript file', function (done) {
-    
+
     var opts = options();
-    
+
     var app = connect()
       .use(superstatic(opts));
-    
+
     request(app)
       .get('/app.js')
       .expect(200)
@@ -91,17 +91,17 @@ describe('serves', function () {
       .expect('Content-Type', 'application/javascript; charset=utf-8')
       .end(done);
   });
-  
+
   it('from custom current working directory', function (done) {
-    
+
     var opts = options();
-    
+
     opts.cwd = join(process.cwd(), '.tmp');
     opts.config.root = './dir';
-    
+
     var app = connect()
       .use(superstatic(opts));
-    
+
     request(app)
       .get('/index.html')
       .expect(200)
@@ -109,11 +109,11 @@ describe('serves', function () {
       .expect('Content-Type', 'text/html; charset=utf-8')
       .end(done);
   });
-  
+
   describe('redirects', function () {
-    
+
     var opts = options();
-    
+
     opts.config.redirects = {
       '/from': '/to',
       '/fromCustom': {
@@ -122,30 +122,30 @@ describe('serves', function () {
       },
       '/external': 'http://redirect.com'
     };
-    
+
     var app = connect()
       .use(superstatic(opts));
-    
+
     it('301', function (done) {
-      
+
       request(app)
         .get('/from')
         .expect(301)
         .expect('Location', '/to')
         .end(done);
     });
-    
+
     it('custom', function (done) {
-      
+
       request(app)
         .get('/fromCustom')
         .expect(302)
         .expect('Location', '/toCustom')
         .end(done);
     });
-    
+
     it('external urls', function (done) {
-      
+
       request(app)
         .get('/external')
         .expect(301)
@@ -153,26 +153,26 @@ describe('serves', function () {
         .end(done);
     });
   });
-  
+
   describe('trailing slash', function () {
-    
-    it('removes trailling slash for file', function (done) {
-      
+
+    xit('removes trailling slash for file', function (done) {
+
       var app = connect()
         .use(superstatic(options()));
-      
+
       request(app)
         .get('/test.html/')
         .expect(301)
         .expect('Location', '/test.html')
         .end(done);
     });
-    
+
     it('add trailing slash with a directory index file', function (done) {
-      
+
       var app = connect()
         .use(superstatic(options()));
-      
+
       request(app)
         .get('/dir')
         .expect(301)
@@ -180,18 +180,18 @@ describe('serves', function () {
         .end(done);
     });
   });
-  
+
   describe('basic auth', function (done) {
-    
+
     it('protects', function (done) {
-      
+
       var opts = options();
-      
+
       opts.protect = 'username:passwords';
-      
+
       var app = connect()
         .use(superstatic(opts));
-        
+
       request(app)
         .get('/')
         .expect(401)
@@ -199,118 +199,118 @@ describe('serves', function () {
         .end(done);
     });
   });
-  
+
   describe('custom headers', function () {
-    
+
     it('with globs', function (done) {
-      
+
       var opts = options();
-      
+
       opts.config.headers = {
         '/**/*.html': {
           'x-custom': 'testing'
         }
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-      
+
       request(app)
         .get('/dir/sub.html')
         .expect('x-custom', 'testing')
         .end(done);
     });
-    
+
     it('exact', function (done) {
-      
+
       var opts = options();
-      
+
       opts.config.headers = {
         '/app.js': {
           'x-custom': 'testing'
         }
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-      
+
       request(app)
         .get('/app.js')
         .expect('x-custom', 'testing')
         .end(done);
     });
   });
-  
-  describe('environment variables', function () {
-    
+
+  xdescribe('environment variables', function () {
+
     it('json', function (done) {
-      
+
       var opts = options();
-      
+
       opts.env = {
         key: 'value'
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-      
+
       request(app)
         .get('/__/env.json')
         .expect({key: 'value'})
         .expect('Content-Type', 'application/json; charset=utf-8')
         .end(done);
     });
-    
+
     it('js', function (done) {
-      
+
       var opts = options();
-      
+
       opts.env = {
         key: 'value'
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-      
+
       request(app)
         .get('/__/env.js')
         .expect(200)
         .expect('Content-Type', 'application/javascript; charset=utf-8')
         .end(done);
     });
-    
+
     it('defaults to .env.json', function (done) {
-      
+
       fs.outputFileSync('.env.json', '{"key":"value"}');
-      
+
       var app = connect()
         .use(superstatic());
-      
+
       request(app)
         .get('/__/env.json')
         .expect({key: 'value'})
         .end(function (err) {
-          
+
           fs.remove('.env.json');
           done(err);
         });
     });
-    
+
     it('serves env file, overriding static routing', function (done) {
-      
+
       var opts = options();
-            
+
       opts.env = {
         key: 'value'
       };
-      
+
       opts.config.routes = {
         '**': 'index.html'
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-      
+
       request(app)
         .get('/__/env.json')
         .expect({key: 'value'})
@@ -318,20 +318,20 @@ describe('serves', function () {
         .end(done);
     });
   });
-  
+
   describe('custom routes', function () {
-    
+
     it('serves file', function (done) {
-      
+
       var opts = options();
-      
+
       opts.config.routes = {
         '/testing': '/index.html'
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-        
+
       request(app)
         .get('/testing')
         .expect(200)
@@ -339,19 +339,19 @@ describe('serves', function () {
         .expect('Content-Type', 'text/html; charset=utf-8')
         .end(done);
     });
-    
+
     it('serves file from custom route when clean urls are on and route matches an html as a clean url', function (done) {
-      
+
       var opts = options();
-      
+
       opts.config.clean_urls = true;
       opts.config.routes = {
         '/testing': '/index.html'
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-        
+
       request(app)
         .get('/testing')
         .expect(200)
@@ -359,53 +359,53 @@ describe('serves', function () {
         .expect('Content-Type', 'text/html; charset=utf-8')
         .end(done);
     });
-    
+
     it('serves static file when no matching route', function (done) {
-      
+
       var opts = options();
-      
+
       opts.config.routes = {
         '/testing': '/index.html'
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-        
+
       request(app)
         .get('/test.html')
         .expect(200)
         .expect('test')
         .end(done);
     });
-    
+
     it('serves with negation', function (done) {
-      
+
       var opts = options();
-      
+
       opts.config.routes = {
         '!/no': '/index.html'
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-        
+
       request(app)
         .get('/no')
         .expect(404)
         .end(done);
     });
-    
+
     it('serves file if url matches exact file path', function (done) {
-      
+
       var opts = options();
-      
+
       opts.config.routes = {
         '**': 'index.html'
       };
-      
+
       var app = connect()
         .use(superstatic(opts));
-        
+
       request(app)
         .get('/test.html')
         .expect(200)
@@ -415,10 +415,10 @@ describe('serves', function () {
   });
 
   it('default favicon', function (done) {
-    
+
     var app = connect()
       .use(superstatic(options()));
-    
+
     request(app)
       .get('/favicon.ico')
       .expect(200)
