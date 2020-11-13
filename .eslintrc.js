723b887cf357e99c231e4b7dfa3b597218724917
Bryan Kendall <bkend@google.com>
2020-11-12 17:14:26 -0800

    better setup of lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,12 @@ module.exports = {
   root: true,
   extends: [
     "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "google",
-    "eslint-config-prettier",
+    "prettier",
+    "prettier/@typescript-eslint",
   ],
   env: {
     es6: true,
@@ -12,6 +16,7 @@ module.exports = {
   parserOptions: {
     sourceType: "module",
     ecmaVersion: 2017,
+    project: ["tsconfig.json"],
   },
   plugins: [
     "prettier",
@@ -22,5 +27,29 @@ module.exports = {
     "require-jsdoc": "warn",
     "prefer-arrow-callback": "error",
   },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-empty-function": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/no-this-alias": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/no-unsafe-assignment": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/no-unsafe-call": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/no-unsafe-member-access": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/no-unsafe-return": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/no-var-requires": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/prefer-regexp-exec": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/restrict-plus-operands": "warn", // TODO(bkendall): remove, allow to error.
+        "@typescript-eslint/unbound-method": "warn", // TODO(bkendall): remove, allow to error.
+      },
+    },
+    {
+      files: ["*.spec.*"],
+      env: {
+        mocha: true,
+      },
+      rules: {},
+    },
+  ],
   parser: "@typescript-eslint/parser",
 }
