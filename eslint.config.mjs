64fd46ae80948f97f52a86dc12091a82c6a3c3a5
Bryan Kendall <bkend@google.com>
2025-09-08 12:01:30 -0700

    upgrade eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,6 +30,7 @@ export default [
       "@typescript-eslint/prefer-includes": "warn", // TODO(bkendall): remove allow to error.
       "@typescript-eslint/prefer-optional-chain": "warn", // TODO(bkendall): remove allow to error.
       "@typescript-eslint/prefer-promise-reject-errors": "warn", // TODO(bkendall): remove allow to error.
+      "@typescript-eslint/prefer-nullish-coalescing": "warn", // TODO(bkendall): remove allow to error.
 
       "no-unused-vars": "off", // Turned off in favor of @typescript-eslint/no-unused-vars.
       "require-atomic-updates": "off", // This rule is so noisy and isn't useful: https://github.com/eslint/eslint/issues/11899
