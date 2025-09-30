ccddefb92eb4bd8c78959235cf5e38677fc0c43a
Bryan Kendall <bkend@google.com>
2025-09-30 16:36:35 -0700

    remove unused rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,7 +27,6 @@ export default [
       "@typescript-eslint/no-require-imports": "warn", // TODO(bkendall): remove allow to error.
       "@typescript-eslint/no-unused-expressions": "warn", // TODO(bkendall): remove allow to error.
       "@typescript-eslint/no-unused-vars": "warn", // TODO(bkendall): remove allow to error.
-      "@typescript-eslint/prefer-includes": "warn", // TODO(bkendall): remove allow to error.
 
       "no-unused-vars": "off", // Turned off in favor of @typescript-eslint/no-unused-vars.
       "require-atomic-updates": "off", // This rule is so noisy and isn't useful: https://github.com/eslint/eslint/issues/11899
